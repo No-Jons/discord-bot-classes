023a5058ad17569d740643b48f40796aa8db0154
No-Jons <61525221+No-Jons@users.noreply.github.com>
2020-10-26 17:24:26 -0700

    Updated base cog

--- ruby/src/cog.rb
+++ ruby/src/cog.rb
@@ -2,23 +2,12 @@ require 'discordrb'
 
 module Cogs
   class Cog
-    @__cog_commands__ = {}
-    @commands_loaded = false
-
     def initialize(name: "Cog")
       @name = name
     end
 
     def name
       @name
-    end
-
-    def get_commands
-      commands = []
-      @__cog_commands__.each do |i|
-        commands.append(i)
-      end
-      commands
     end
 
   end
